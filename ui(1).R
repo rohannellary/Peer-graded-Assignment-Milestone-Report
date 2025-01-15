ac4b32f692526a8ac06dc91298b1aecfa1cdd975
rohannellary <rohannellary@gmail.com>
2025-01-15 13:12:56 +0530

    Add files via upload

---
+++ ui(1).R
@@ -0,0 +1,22 @@
+library(shiny)
+shinyUI(   fluidPage(
+  headerPanel("Next Word Prediction App"
+  ),
+  sidebarPanel(
+    h3("Introducton"),
+    p("This application predicts the next possible word in a phrase or sentence. To use it, simply type word(s) on the text field on the screen
+      and up to 4 possible next words will display in buttons below the field. Click on your intended match to add it to the field."),
+    p("The application uses natural language processing, namely, n-grams, Markov model, and Katz's back-off model to perform text prediction."),
+    p("This N-gram Word Predictor was developed as a capstone project to complete the Johns Hopkins data science Course.")
+    ),
+  mainPanel(
+    h3("Input"),
+    textInput("inputTxt", "Type in word(s) below:", width = "90%"),
+    uiOutput("words"),
+    br(),
+    wellPanel(
+      h4("Details")
+    )
+    
+  )
+))
